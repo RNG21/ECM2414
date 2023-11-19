cba251b504d198cd97b10c80be9ac762b4fb8d3e
RNG21 <frankytsang921@gmail.com>
2023-11-19 01:45:37 +0000

    Pack validation

--- Main.java
+++ Main.java
@@ -1,6 +1,13 @@
+import src.Pack;
+import src.Card;
+
 public class Main {
     public static void main(String[] args) {
-
+        try {
+            Pack pack = Pack.generatePack(0);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
   }
 
